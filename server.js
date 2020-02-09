7afdf7f7b76b54a0a35afc9fb98e506ce2ab81e5
Stephen Reynolds <sdreynolds110@gmail.com>
2020-02-08 21:53:57 -0500

    created tasks for first prompt

--- server.js
+++ server.js
@@ -9,3 +9,66 @@ var connection = mysql.createConnection({
   password: "",
   database: "employees_DB"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    firstPrompt();
+  });
+  
+  function firstPrompt() {
+  
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          "View Employees by Manager",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          "Remove Role",
+          "Update Employee Manager",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+          case "View Department Salaries":
+            viewDepartmentSalaries();
+            break;
+          case "View Employees by Manager":
+            viewEmployeeByManager();
+            break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "Remove Employee":
+            removeEmployees();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          case "Remove Role":
+            removeRole();
+            break;
+          case "Update Employee Manager":
+            updateEmployeeManager();
+            break;
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
