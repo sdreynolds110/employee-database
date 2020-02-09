c9f6457202b0bec916b21ccaf0e7539698b07129
Stephen Reynolds <sdreynolds110@gmail.com>
2020-02-08 21:33:27 -0500

    created server file and began connections

---
+++ server.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employees_DB"
+});
